368a1c2c63279b308447919fa5770ada5623dceb
Rahmat Subandi <rahmat@indonesiacrypto.network>
2022-06-27 17:31:05 +0700

    add backend only

--- frontend/utils/utils.js
+++
@@ -1,8 +0,0 @@
-export function formatDate(datestring) {
-  return new Date(datestring).toLocaleDateString("en-US", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-}
