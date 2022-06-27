7cb2b0dc23c7654c35b6cdde3f33021930106e54
Restyled.io <commits@restyled.io>
2022-06-27 03:43:28 +0000

    Restyled by clang-format

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -5,17 +5,19 @@
  * to run scheduled jobs.
  *
  * The cron format consists of:
- * [SECOND (optional)] [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK]
+ * [SECOND (optional)] [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF
+ * WEEK]
  *
- * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#cron-tasks
+ * See more details here:
+ * https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#cron-tasks
  */
 
 module.exports = {
-  /**
-   * Simple example.
-   * Every monday at 1am.
-   */
-  // '0 1 * * 1': () => {
-  //
-  // }
+    /**
+     * Simple example.
+     * Every monday at 1am.
+     */
+    // '0 1 * * 1': () => {
+    //
+    // }
 };
